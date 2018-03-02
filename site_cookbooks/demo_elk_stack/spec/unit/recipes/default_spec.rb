26d1e9936b2b8311ae31d1fae39da9d4a654ff1c
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2018-03-02 16:36:37 -0500

    updating the names in cookbook names

--- site_cookbooks/demo_elk_stack/spec/unit/recipes/default_spec.rb
+++ site_cookbooks/demo_elk_stack/spec/unit/recipes/default_spec.rb
@@ -6,7 +6,7 @@
 
 require 'spec_helper'
 
-describe 'ELK_Stack::default' do
+describe 'demo_elk_stack::default' do
   context 'When all attributes are default, on Ubuntu 16.04' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
