30fb94ea41efc28bf73c051b9b506823189cec3f
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2018-03-13 11:03:11 -0400

    trying to fix attribute issues

--- site_cookbooks/demo_gitlab/attributes/default.rb
+++ site_cookbooks/demo_gitlab/attributes/default.rb
@@ -1,17 +1,17 @@
-node.default['gitlab']['endpoint'] = "http://#{node['hostname']}/"
-node.default['gitlab']['runner_endpoint'] = "http://#{node['hostname']}/"
-node.default['gitlab']['gitlab_root_pwd'] = 'superman'
+default['gitlab']['endpoint'] = "http://#{node['hostname']}/"
+default['gitlab']['runner_endpoint'] = "http://#{node['hostname']}/"
+default['gitlab']['gitlab_root_pwd'] = 'superman'
 
-if node['ec2']['private_ip_address']
-    node.default['gitlab']['endpoint'] = "http://#{node['ec2']['private_ip_address']}/"
-    node.default['gitlab']['runner_endpoint'] = "http://#{node['ec2']['private_ip_address']}/"
+if attribute?('ec2')
+    default['gitlab']['endpoint'] = "http://#{node['ec2']['private_ip_address']}/"
+    default['gitlab']['runner_endpoint'] = "http://#{node['ec2']['private_ip_address']}/"
 end
 
 # SETUP VARIABLES FOR GITLAB.RB CONFIGURATION
-node.default['omnibus-gitlab']['gitlab_rb']['external_url'] = node['gitlab']['endpoint']
-node.default['omnibus-gitlab']['gitlab_rb']['nginx']['listen_port'] = 80
-node.default['omnibus-gitlab']['gitlab_rb']['nginx']['listen_https'] = false
-node.default['omnibus-gitlab']['gitlab_rb']['nginx']['proxy_set_headers'] = {
+default['omnibus-gitlab']['gitlab_rb']['external_url'] = node['gitlab']['endpoint']
+default['omnibus-gitlab']['gitlab_rb']['nginx']['listen_port'] = 80
+default['omnibus-gitlab']['gitlab_rb']['nginx']['listen_https'] = false
+default['omnibus-gitlab']['gitlab_rb']['nginx']['proxy_set_headers'] = {
     "X-Forwarded-Proto" => "https",
     "X-Forwarded-Ssl" => "on"
 }
@@ -29,17 +29,27 @@ node.default['omnibus-gitlab']['gitlab_rb']['nginx']['proxy_set_headers'] = {
 # SET RUNNER SETTINGS
 #node.default['gitlab-runner']['env']['CI_SERVER_URL'] = gitlab_server
 #node.default['gitlab-runner']['env']['REGISTRATION_TOKEN'] = gitlab_token
-node.default['gitlab-runner']['env']['RUNNER_NAME'] = node['hostname']
-node.default['gitlab-runner']['env']['REGISTER_NON_INTERACTIVE'] = 'true' # DON'T CHANGE THIS
+default['gitlab-runner']['env']['RUNNER_NAME'] = node['hostname']
+default['gitlab-runner']['env']['REGISTER_NON_INTERACTIVE'] = 'true' # DON'T CHANGE THIS
 
 
-node.default['gitlab-runner']['env']['RUNNER_EXECUTOR'] = 'docker' #SET RUNNER EXECUTOR
+default['gitlab-runner']['env']['RUNNER_EXECUTOR'] = 'docker' #SET RUNNER EXECUTOR
 
 # CONFIGURE DOCKER SETTINGS
-node.default['gitlab-runner']['env']['DOCKER_IMAGE'] = 'ubuntu'
-node.default['gitlab-runner']['env']['REGISTER_LOCKED'] = 'false'
-node.default['gitlab-runner']['env']['REGISTER_RUN_UNTAGGED'] = 'true'
-node.default['gitlab-runner']['env']['RUNNER_TAG_LIST'] = "mu-node, #{node['hostname']}, #{node['platform_family']}, docker"
-node.default['gitlab-runner']['env']['RUNNER_EXECUTOR'] = 'docker'
-node.default['gitlab-runner']['env']['RUNNER_REQUEST_CONCURRENCY'] = '100'
-node.default['gitlab-runner']['env']['RUNNER_LIMIT'] = '99'
+default['gitlab-runner']['env']['DOCKER_IMAGE'] = 'ubuntu'
+default['gitlab-runner']['env']['REGISTER_LOCKED'] = 'false'
+default['gitlab-runner']['env']['REGISTER_RUN_UNTAGGED'] = 'true'
+default['gitlab-runner']['env']['RUNNER_TAG_LIST'] = "mu-node, #{node['hostname']}, #{node['platform_family']}, docker"
+default['gitlab-runner']['env']['RUNNER_EXECUTOR'] = 'docker'
+default['gitlab-runner']['env']['RUNNER_REQUEST_CONCURRENCY'] = '100'
+default['gitlab-runner']['env']['RUNNER_LIMIT'] = '99'
+
+# ONlY SET THESE IF NOTHING IS SET EXPLICITY
+if !attribute?('runner_token')
+    puts "###################### Generating Runner Token ######################"
+    override['gitlab']['runner_token'] = SecureRandom.urlsafe_base64
+end
+if !attribute?('gitlab_root_pwd')
+    puts "###################### Generating Root Password ######################"
+    override['gitlab']['gitlab_root_pwd'] = SecureRandom.urlsafe_base64
+end
