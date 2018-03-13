6a27e7b29eb86d62c6c3b5f0e979be6b5bd10c49
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2018-03-13 11:16:54 -0400

    crazy

--- site_cookbooks/demo_gitlab/recipes/default.rb
+++ site_cookbooks/demo_gitlab/recipes/default.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-require 'securerandom'
+# require 'securerandom'
 include_recipe 'chef-vault'
 
 node.override['gitlab']['is_server'] = true
