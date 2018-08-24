ae18db02dcb74b2addd04a62e7cd3464b47ca454
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2018-08-24 14:25:16 -0400

    add logic back

--- cookbooks/mu-demo-mu/recipes/default.rb
+++ cookbooks/mu-demo-mu/recipes/default.rb
@@ -4,24 +4,24 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-# remote_file 'Download Mu Installer' do
-# 	source 'https://raw.githubusercontent.com/cloudamatic/mu/master/cookbooks/mu-master/recipes/init.rb'
-# 	path "#{Chef::Config[:file_cache_path]}/installer.rb"
-# 	mode '0755'
-# 	action :create
-# end
+remote_file 'Download Mu Installer' do
+	source 'https://raw.githubusercontent.com/cloudamatic/mu/master/cookbooks/mu-master/recipes/init.rb'
+	path "#{Chef::Config[:file_cache_path]}/init.rb"
+	mode '0755'
+	action :create
+end
 
 hostname = node['ec2']['public_hostname'] || node['ec2']['private_dns_name']
 
-# execute 'Install Mu' do
-# 	command "/opt/chef/bin/chef-apply #{Chef::Config[:file_cache_path]}/init.rb -n -m mu@egt-labs.com -u 'mu master' -h #{node['hostname']} -p #{hostname}"
-# 	live_stream true
-# end
+execute 'Install Mu' do
+	command "/opt/chef/bin/chef-apply #{Chef::Config[:file_cache_path]}/init.rb"
+	live_stream true
+end
 
-# execute 'Install Mu' do
-# 	command "/opt/mu/bin/mu-configure -n -m mu@egt-labs.com -u 'mu master' -h #{node['hostname']} -p #{hostname}"
-# 	live_stream True
-# end
+execute 'Install Mu' do
+	command "/opt/mu/bin/mu-configure -n -m mu@egt-labs.com -u 'mu master' -h #{node['hostname']} -p #{hostname}"
+	live_stream True
+end
 
 # execute 'create_user' do
 # 	command <<-EOH
