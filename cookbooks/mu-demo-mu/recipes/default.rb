04b94807064aa9cdab27a929571d91c1b2fb25d1
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2018-08-24 12:30:58 -0400

    pass variables to the configure

--- cookbooks/mu-demo-mu/recipes/default.rb
+++ cookbooks/mu-demo-mu/recipes/default.rb
@@ -12,5 +12,13 @@ remote_file 'Download Mu Installer' do
 end
 
 execute 'Install Mu' do
-	command "#{Chef::Config[:file_cache_path]}/installer"
+	command "#{Chef::Config[:file_cache_path]}/installer -n -m mu@egt-labs.com -u 'mu master' -h #{node.name} -p #{node.ip}"
 end
+
+# execute 'create_user' do
+# 	command <<-EOH
+#     	ip=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+# 		admin_email="mu@eg"
+# 		sed -i "s/\/opt\/mu\/bin\/mu-configure \$\@/\/opt\/mu\/bin\/mu-configure \$\@ -np $ip -m $admin_email/g" installer""" % branch
+#     EOH
+#   end
